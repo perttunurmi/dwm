de370d205b9ed404e740aa9719297087f8076115
PERTZ42 <perttu.nurmi+github@gmail.com>
2024-10-23 18:17:11 +0300

    alacritty --> gnome terminal

--- config.h
+++ config.h
@@ -67,7 +67,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "alacritty", NULL };
+static const char *termcmd[]  = { "gnome-terminal", NULL };
 static const char *files[]    = { "nautilus", NULL };
 static const char *bridown[]  = { "brightnessctl", "s", "75-", NULL};
 static const char *briup[]  = { "brightnessctl", "s", "75+", NULL};
