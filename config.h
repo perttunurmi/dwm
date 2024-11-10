66cff675d3cf1162290174338367556e47712d34
perttunurmi <perttu.nurmi@gmail.com>
2024-11-10 11:14:58 +0200

    color revert, better monitor switching

--- config.h
+++ config.h
@@ -19,8 +19,7 @@ static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-/*static const char col_cyan[]        = "#005577";*/
-static const char col_cyan[]        = "#567d54";
+static const char col_cyan[]        = "#005577";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -69,9 +68,9 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *xrandr[] = { "xrandr", "--output", "eDP-1", "--auto", "--output", "HDMI-2", "--auto", "--output", "HDMI-1", "--auto", NULL };
+static const char *xrandr[] = { "xrandr", "--output", "eDP-1", "--auto", "--output", "HDMI-2", "--auto", "--output", "HDMI-1", "--auto", "--output", "DP-1", "--auto", "--output", "DP-2", "--auto", NULL };
 static const char *rofi[] = { "rofi", "-show", "drun", NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "kitty", NULL };
 static const char *printscreen[]  = { "flameshot", "gui" , NULL };
 static const char *files[]    = { "thunar", NULL };
 static const char *bridown[]  = { "brightnessctl", "s", "75-", NULL};
